76281fac2a3744f9b2d17d7abccccbd17b03562b
Paul Ralyea <047par@gmail.com>
2020-03-05 05:58:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,7 +4,7 @@ second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-quotient = first_number // second_number
+quotient = first_number  second_number
 
 
 
